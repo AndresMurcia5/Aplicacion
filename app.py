06ee9d49a3b24f107d550b316de103a012e6e0ec
Sala 203 <sala203@ujaverianas.loc>
2025-04-11 14:23:44 -0500

    hola

--- app.py
+++ app.py
@@ -12,24 +12,24 @@ tab1, tab2 = st.tabs(['Tab 1', 'Tab 2'])
 
 with tab1:
      # análisis univariado
-     fig, ax= plt.subplots(1, 3, figsize=(10, 4), color='red')
+     fig, ax= plt.subplots(1, 3, figsize=(10, 4))
 
      # educ 
      tab_freq = ran['educ'].value_counts().sort_index()
-     ax[0].bar(tab_freq.index, tab_freq.values)
+     ax[0].bar(tab_freq.index, tab_freq.values, color = 'red')
      # edad
-     ax[1].hist(ran['edad'], bins=30, color='red')
+     ax[1].hist(ran['edad'], bins=30, color = 'orange')
      # wage
-     ax[2].hist(ran['wage'], bins=40, color='red')
+     ax[2].hist(ran['wage'], bins=40, color = 'green')
 
      st.pyplot(fig)
 
      # análisis bivariado
      fig, ax = plt.subplots(1, 2, figsize=(10,4))
      #educ vs. wage
-     ax[0].scatter(ran['educ'],ran['wage'])
+     ax[0].scatter(ran['educ'],ran['wage'], color = 'salmon')
      #edad vs. wage
-     ax[1].scatter(ran['edad'],ran['wage'])
+     ax[1].scatter(ran['edad'],ran['wage'], color = 'black')
 
      st.pyplot(fig)
 
